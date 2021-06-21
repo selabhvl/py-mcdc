528035cc3510c8a405149001a223d835bdf79346
Volker Stolz <vsto@hvl.no>
2021-06-21 18:46:08 +0200

    Tweak. Should be right (despite the `*foo`), but probably there's also a test-case missing that asserts what we expected, as well as a comment.

--- mcdc_test/comparePlotResults.py
+++ mcdc_test/comparePlotResults.py
@@ -1,9 +1,11 @@
-h1=[1, 2, 3, 1, 2, 6, 3], [2, 4, 5, 8, 0, 5, 18], [1, 1, 1, 8, 0, 140, 3], [0, 1, 0, 8, 0, 10, 13]
-h2=[1,2,3],[2,4,5],[1,1,1]
+h1=[[1, 2, 3, 1, 2, 6, 3], [2, 4, 5, 8, 0, 5, 18], [1, 1, 1, 8, 0, 140, 3], [0, 1, 0, 8, 0, 10, 13]]
+h2=[[1,2,3],[2,4,5],[1,1,1]]
+
+
 def compareresult(hs):
     BoolTF =list(map(lambda vals: list(map(lambda v: v == min(vals), vals)), (zip(*hs))))
     comparison = (list(map(lambda x: sum(x), zip(*BoolTF))))
     return comparison
 
-Result = compareresult(h2)
-print(Result)
+# Result = compareresult(h2)
+# print(Result)
