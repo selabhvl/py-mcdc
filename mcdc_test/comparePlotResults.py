5a97afe0ccfb3d81188cfb56396dd8450de88242
nachorequeno <nrequeno@unizar.es>
2021-07-03 22:51:33 +0200

    Addition of '<' and '<=' comparisons in comparePlotResults.py

--- mcdc_test/comparePlotResults.py
+++ mcdc_test/comparePlotResults.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+from itertools import permutations
 from vsplot import load_resultMap
 
 
@@ -8,18 +10,58 @@ def compareresult(hs):
     return list(comparison)
 
 
-def compareresult_using_list_comprh(hs):
+def results_better_equal(hs, criteria=min):
     # Zip groups the 1st experiment of all heuristics, then the 2nd experiment, and so on.
     # hs = [h_1, h_2, ..., h_n] where
-    # h_i = [run_i_1, run_i_2, ..., run_i_n]
+    # h_i = [run_i_1, run_i_2, ..., run_i_m]
     # Therefore,
-    # zip(*hs) = [run_1, run_2, ..., run_n] where
+    # zip(*hs) = [run_1, run_2, ..., run_m] where
     # run_1 = [run_1_i for all h_i in hs]
 
-    set_of_runs = ([hi == min(run_j) for hi in run_j] for run_j in zip(*hs))
-    num_times_hi_is_best = [sum(hi_is_best) for hi_is_best in zip(*set_of_runs)]
+    set_of_runs = ([hi == criteria(run_j) for hi in run_j] for run_j in zip(*hs))
+    num_times_hi_is_better_equal = [sum(hi_is_best) for hi_is_best in zip(*set_of_runs)]
 
-    return num_times_hi_is_best
+    return num_times_hi_is_better_equal
+
+
+def results_better(hs):
+    # type: (list) -> (list, list)
+    # hs = [h_1, h_2, ..., h_n] are the results of running n heuristics, where
+    # h_i = [run_i_1, run_i_2, ..., run_i_m]
+
+    hs_array = map(np.array, hs)
+    # permutations('ABCD', 2) == [AB AC AD BA BC BD CA CB CD DA DB DC]
+    per = permutations(hs_array, 2)
+    return [sum(np.less(h_i, h_j)) for (h_i, h_j) in per]
+
+
+def results_equal(hs):
+    # type: (list) -> (list, list)
+    # hs = [h_1, h_2, ..., h_n] are the results of running n heuristics, where
+    # h_i = [run_i_1, run_i_2, ..., run_i_m]
+
+    hs_array = map(np.array, hs)
+    # permutations('ABCD', 2) == [AB AC AD BA BC BD CA CB CD DA DB DC]
+    per = permutations(hs_array, 2)
+    return [sum(np.equal(h_i, h_j)) for (h_i, h_j) in per]
+
+
+def results_better_two_heuristics(hs):
+    # type: (list) -> (list, list)
+    # hs = [h_1, h_2] are the results of running two heuristics, where
+    # h_i = [run_i_1, run_i_2, ..., run_i_n]
+
+    h1, h2 = np.array(hs[0]), np.array(hs[1])
+    return [sum(h1 < h2), sum(h2 < h1)]
+
+
+def results_equal_two_heuristics(hs):
+    # type: (list) -> (list, list)
+    # hs = [h_1, h_2] are the results of running two heuristics, where
+    # h_i = [run_i_1, run_i_2, ..., run_i_n]
+
+    h1, h2 = np.array(hs[0]), np.array(hs[1])
+    return [sum(h1 == h2), sum(h2 == h1)]
 
 
 def extract_results(resultMap):
@@ -40,5 +82,9 @@ if __name__ == "__main__":
 
     # Compare heuristics h_1 and h_2
     heuristics = [h_1, h_2]
-    res = compareresult(heuristics)
-    print("H1 better than H2: {0}\nH2 better than H1: {1}".format(res[0], res[1]))
+    better_equal = results_better_equal(heuristics)
+    better = results_better(heuristics)
+    equal = results_equal(heuristics)
+    print("H1 better or equal than H2: {0}\nH2 better or equal than H1: {1}".format(better_equal[0], better_equal[1]))
+    print("H1 better than H2: {0}\nH2 better than H1: {1}".format(better[0], better[1]))
+    print("H1 equal than H2: {0}\nH2 equal than H1: {1}".format(equal[0], equal[1]))
