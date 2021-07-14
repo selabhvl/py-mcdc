8be96c82072aff69be24160b45e17b9dfcc37600
fahishakiye <Faustin.Ahishakiye@hvl.no>
2021-07-14 08:16:32 +0200

    number of conditions in hand made decisions

--- mcdc_test/handmade.py
+++ mcdc_test/handmade.py
@@ -1,9 +1,13 @@
 from pyeda.inter import *
 a, b, c, d, e, f, g, h, i, j, k = map(bddvar, 'abcdefghij')
-D1 = a & b & c | d & e & f | g & h & a
-D2 = a & b & c | c & d & e | e & f & g
-D3 = a & b | b & c | c & d | d & a
-D4 = a & f & ~c & d & ~e | ~a & f & h & i & j | b & ~g & ~c & ~d & ~e | ~b & g & ~h & ~i & ~j
-D5 = b & ~g & ~c & ~d & ~e | ~a & c & d & e
+D21 = a & b & c | d & e & f | g & h & a
+D22 = a & b & c | c & d & e | e & f & g
+D23 = a & b | b & c | c & d | d & a
+D24 = a & f & ~c & d & ~e | ~a & f & h & i & j | b & ~g & ~c & ~d & ~e | ~b & g & ~h & ~i & ~j
+D25 = b & ~g & ~c & ~d & ~e | ~a & c & d & e
 
 hdmd = [D1, D2, D3, D4, D5]
+hdmd_names = ["D2"+str(i) for i in range(1, 6)]
+hdmd_num_cond = [8, 7, 4, 10, 5]
+#hdmd_dict_name = {ds_i: value for ds_i, value in zip(hdmd_names,  hdmd_num_cond)}
+hdmd_dict = dict(zip(hdmd, hdmd_num_cond))
