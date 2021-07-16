6653084c866acf716268692eba9cae049fdcdd28
fahishakiye <Faustin.Ahishakiye@hvl.no>
2021-07-16 13:08:33 +0200

    corrected the error form hand made examples

--- mcdc_test/handmade.py
+++ mcdc_test/handmade.py
@@ -1,5 +1,5 @@
 from pyeda.inter import *
-a, b, c, d, e, f, g, h, i, j, k = map(bddvar, 'abcdefghij')
+a, b, c, d, e, f, g, h, i, j, k = map(bddvar, 'abcdefghijk')
 D21 = a & b & c | d & e & f | g & h & a
 D22 = a & b & c | c & d & e | e & f & g
 D23 = a & b | b & c | c & d | d & a
@@ -8,6 +8,7 @@ D25 = b & ~g & ~c & ~d & ~e | ~a & c & d & e
 
 hdmd = [D21, D22, D23, D24, D25]
 hdmd_names = ["D2"+str(i) for i in range(1, 6)]
+print(hdmd_names)
 hdmd_num_cond = [8, 7, 4, 10, 5]
 #hdmd_dict_name = {ds_i: value for ds_i, value in zip(hdmd_names,  hdmd_num_cond)}
 hdmd_dict = dict(zip(hdmd, hdmd_num_cond))
